dd55fd68f02f80ddd621e2a518d49fefcf955cf4
MichaelNeum <michael.neumayr@gmx.net>
2020-06-15 17:10:19 +0200

    extract voltage gauge

--- ArduinoDMM/ArduinoDMM/MainWindow.xaml.cs
+++ ArduinoDMM/ArduinoDMM/MainWindow.xaml.cs
@@ -26,29 +26,29 @@ namespace ArduinoDMM
         public MainWindow()
         {
             InitializeComponent();
-            serial.BaudRate = 9600;
-            serial.PortName = "COM4";
-            try { serial.Open(); }
-            catch { }
-            serial.DataReceived += Serial_DataReceived;
+            //serial.BaudRate = 9600;
+            //serial.PortName = "COM4";
+            //try { serial.Open(); }
+            //catch { }
+            //serial.DataReceived += Serial_DataReceived;
         }
-        private delegate void nextStep(string Data);
-        private void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        {
-            string Data = serial.ReadLine();
-            Dispatcher.Invoke(DispatcherPriority.Send, new nextStep(SerialEncoder), Data);
-        }
-        private void SerialEncoder(string Data)
-        {
-            SerialLib values = new SerialLib(Data);
-            string command = values.getCommand;
-            int number = values.getNumber;
-            if (command.Equals("VoltDeg")) RectManipulation(number);
-            else return;
-        }
-        private void RectManipulation(int angle)
-        {
-            PointerRect.RenderTransform = new RotateTransform(angle, PointerRect.Width, PointerRect.Height / 2);
-        }
+        //private delegate void nextStep(string Data);
+        //private void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        //{
+        //    string Data = serial.ReadLine();
+        //    Dispatcher.Invoke(DispatcherPriority.Send, new nextStep(SerialEncoder), Data);
+        //}
+        //private void SerialEncoder(string Data)
+        //{
+        //    SerialLib values = new SerialLib(Data);
+        //    string command = values.getCommand;
+        //    int number = values.getNumber;
+        //    if (command.Equals("VoltDeg")) RectManipulation(number);
+        //    else return;
+        //}
+        //private void RectManipulation(int angle)
+        //{
+        //    PointerRect.RenderTransform = new RotateTransform(angle, PointerRect.Width, PointerRect.Height / 2);
+        //}
     }
 }
