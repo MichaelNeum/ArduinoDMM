068df413de436757155e96cf6482081a776e1b45
MichaelNeum <michael.neumayr@gmx.net>
2020-07-12 16:32:17 +0200

    Add reistance measurement - not working yet

--- ArduinoDMM/ArduinoDMM.ino
+++ ArduinoDMM/ArduinoDMM.ino
@@ -11,10 +11,25 @@ void setup()
 void loop() 
 {
   int voltage = map(analogRead(definitions::VoltagePin), 0, 1023, 0, 180);
-  String message = "VoltDeg/" + String(voltage);
+  double resistorVoltage1 = map(analogRead(definitions::ResistorPin1), 0.0, 1023.0, 0.0, 5.0);
+  double resistorVoltage2 = map(analogRead(definitions::ResistorPin2), 0.0, 1023.0, 0.0, 5.0);
+  Serial.println(resistorVoltage1);
+  Serial.println(resistorVoltage2);
+  double resistance = 0.0;
+  if(resistorVoltage1 > 0) resistance = resistorVoltage2 * definitions::BuiltInResistor / resistorVoltage1;
   if(millis()%definitions::WaitTime < definitions::WaitTime/2){
-    if(notSent) Serial.println(message);
+    if(notSent){ 
+      Serial.println(createMessage(voltage, 'V'));
+      Serial.println(createMessage(resistance, 'r'));
+    }
     notSent = false;
   }
   else notSent = true;
+}
+
+template<typename T>
+String createMessage(T value, char type) {
+  if(type == 'r') return "Res/" + String(value);
+  if(type == 'V') return "VoltDeg/" + String(value);
+  return "";
 }
--- ArduinoDMM/namespaces.h
+++ ArduinoDMM/namespaces.h
@@ -1,5 +1,8 @@
 namespace definitions
 {
   constexpr int VoltagePin = A0;
-  constexpr int WaitTime = 50;
+  constexpr int ResistorPin1 = A1;
+  constexpr int ResistorPin2 = A2;
+  constexpr int WaitTime = 500;
+  constexpr int BuiltInResistor = 1000;
 }
